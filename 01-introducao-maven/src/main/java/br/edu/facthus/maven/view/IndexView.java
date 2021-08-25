a8943a0b003f98055c9d8489ca14e38eff829a82
Fernando Mattioli <mattioli.fernando@gmail.com>
2021-08-25 19:43:14 -0300

    Atualiza o projeto "Introdução ao Maven"

--- 01-introducao-maven/src/main/java/br/edu/facthus/maven/view/IndexView.java
+++ 01-introducao-maven/src/main/java/br/edu/facthus/maven/view/IndexView.java
@@ -11,21 +11,6 @@ public class IndexView {
 	
 	private LocalDateTime dthrAtual = LocalDateTime.now();
 	
-	private String nome;
-	
-	public String getMensagem() {
-		String novoNome = "";
-		if (nome != null)
-			novoNome = nome;
-		
-		if (dthrAtual.getHour() < 12)
-			return String.format("Bom dia %s! Seja bem vindo(a)!", novoNome);
-		else if (dthrAtual.getHour() < 18)
-			return String.format("Boa tarde %s! Seja bem vindo(a)!", novoNome);
-		else
-			return String.format("Boa noite %s! Seja bem vindo(a)!", novoNome);
-	}
-	
 	public LocalDateTime getDthrAtual() {
 		System.out.println(dthrAtual);
 		return dthrAtual;
@@ -34,13 +19,5 @@ public class IndexView {
 	public void setDthrAtual(LocalDateTime dthrAtual) {
 		this.dthrAtual = dthrAtual;
 	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
+	
 }
